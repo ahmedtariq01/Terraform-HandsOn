badbf25ccc0c10575098da0378660f28ea2aebcc
Ahmed Tariq <ahmed.tm097@gmail.com>
2022-08-25 16:42:39 +0500

    Creating bucket

--- Basic/main.tf
+++ Basic/main.tf
@@ -1,11 +1,38 @@
+# provider: aws
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.27.0"
+    }
+  }
+}
+
+# Configuration of the AWS provider
 provider "aws" {
-
-  version    = "~> 4.27.0"
-  region     = "us-east-1"
-  access_key = var.Access_key
-  secret_key = var.Secret_key
-
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+  
 }
 
 
-  
+# creating a S3 bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = "My first bucket"
+    Environment = "Dev"
+  }
+}
+
+# creating a S3 bucket policy
+resource "aws_s3_bucket_acl" "my_bucket_policy" {
+  bucket = aws_s3_bucket.my_bucket.id
+  acl    = "private"
+}
+
+
+
+
