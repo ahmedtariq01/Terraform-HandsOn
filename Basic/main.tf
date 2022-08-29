f05bebc3807b8444a482c633aca20f62f95ee15b
Ahmed Tariq <ahmed.tm097@gmail.com>
2022-08-29 18:19:01 +0500

    Adding Ec2

--- Basic/main.tf
+++ Basic/main.tf
@@ -8,31 +8,45 @@ terraform {
   }
 }
 
+# local variable
+locals {
+  team = "Dev"
+}
+
+
 # Configuration of the AWS provider
 provider "aws" {
   region     = var.region
   access_key = var.access_key
   secret_key = var.secret_key
-  
+
 }
 
 
-# creating a S3 bucket
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
+# # creating a S3 bucket
+# resource "aws_s3_bucket" "my_bucket" {
+#   bucket = var.bucket_name
+
+#   tags = {
+#     Name        = "My first bucket"
+#     Environment = "Dev"
+#   }
+# }
+
+# # creating a S3 bucket policy
+# resource "aws_s3_bucket_acl" "my_bucket_policy" {
+#   bucket = aws_s3_bucket.my_bucket.id
+#   acl    = "private"
+# }
+
+# ec2 instance
+resource "aws_instance" "web" {
+  ami           = var.instance_image           # Amazone Machine Image
+  instance_type = "t1.micro"
 
   tags = {
-    Name        = "My first bucket"
-    Environment = "Dev"
+    Name = var.instance_name
   }
 }
-
-# creating a S3 bucket policy
-resource "aws_s3_bucket_acl" "my_bucket_policy" {
-  bucket = aws_s3_bucket.my_bucket.id
-  acl    = "private"
-}
-
-
 
 
