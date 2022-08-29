b6d0153e9df7362f74c361b8b25af745c4d5c528
Ahmed Tariq <ahmed.tm097@gmail.com>
2022-08-29 18:31:05 +0500

    Adding loacl variable

--- Basic/main.tf
+++ Basic/main.tf
@@ -23,29 +23,30 @@ provider "aws" {
 }
 
 
-# # creating a S3 bucket
-# resource "aws_s3_bucket" "my_bucket" {
-#   bucket = var.bucket_name
+# creating a S3 bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
 
-#   tags = {
-#     Name        = "My first bucket"
-#     Environment = "Dev"
-#   }
-# }
+  tags = {
+    Name        = "My first bucket"
+    Environment = local.team
+  }
+}
 
-# # creating a S3 bucket policy
-# resource "aws_s3_bucket_acl" "my_bucket_policy" {
-#   bucket = aws_s3_bucket.my_bucket.id
-#   acl    = "private"
-# }
+# creating a S3 bucket policy
+resource "aws_s3_bucket_acl" "my_bucket_policy" {
+  bucket = aws_s3_bucket.my_bucket.id
+  acl    = "private"
+}
 
 # ec2 instance
 resource "aws_instance" "web" {
-  ami           = var.instance_image           # Amazone Machine Image
+  ami           = var.instance_image # Amazone Machine Image
   instance_type = "t1.micro"
 
   tags = {
-    Name = var.instance_name
+    Name        = var.instance_name
+    Environment = local.team
   }
 }
 
